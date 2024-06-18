36c4689d6b3d7707bac44b21b4f061a09a470e98
Justyna Leo <justyna.leoniuk@mycit.ie>
2024-06-18 22:32:00 +0100

    adding the buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,12 +2,16 @@
 
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link';
 import { type } from 'os';
 import React from 'react'
+import {BsArrowRight, BsGithub, BsLinkedin} from "react-icons/bs"
+import { HiDownload } from "react-icons/hi";
+
 
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div
@@ -29,13 +33,55 @@ export default function Intro() {
           </motion.div>
         </div>
       </div>
-      <p>
+      <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+      initial={{opacity:0, y:100}}
+      animate={{opacity:1, y:0}}
+      >
         <span className='font-bold'>My name is Justyna. </span>I'm a {" "}
         <span className='font-bold'>computer science student. </span> I love learning about{" "}
-        <span className='italic'>Cybersecurity & the cloud. </span>
+        <span className='italic underline'>Cybersecurity & the cloud. </span>
 
-      </p>
+      </motion.p>
+
+
+
+
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium '
+      initial={{opacity: 0, y: 100}}
+      animate={{opacity: 1, y:0}}
+      transition={{delay: 0.1}}>
+      <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition
+          ">
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-[#ede9fe] px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack shadow-lg"
+          href="/CV.pdf"
+          download>
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+      <a className=' bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+      href="https://www.linkedin.com/in/justyna-leoniuk-386b7719a/">
+        <BsLinkedin />
+      </a>
+
+
+      <a className=' bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+      href = "https://github.com/KurapikaLeo">
+        <BsGithub />
+      </a>
+
+      </motion.div>
+
+
+
     </section>
-  )
-}
+  );
 
+  }
